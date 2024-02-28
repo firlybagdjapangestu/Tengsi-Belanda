a1ce7c481138dd139eb9a74ede9a3e73bff48a09
firlybagdjapangestu <57309957+firlybagdjapangestu@users.noreply.github.com>
2024-02-28 17:15:54 +0700

    Upload Project

---
+++ Tengsi Belanda/Assets/Script/InputGaze.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputGaze : MonoBehaviour
+{
+    [SerializeField] private int raycastDistance;
+    [SerializeField] private RaycastHit raycastHit;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        if(Physics.Raycast(ray,out raycastHit, raycastDistance))
+        {
+            if (raycastHit.transform.CompareTag("Fire"))
+            {
+                Destroy(raycastHit.transform.gameObject);
+            }
+        }
+    }
+}
---
+++ Tengsi Belanda/Assets/Script/InteractionWithUi.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class InteractionWithUi : MonoBehaviour
+{
+    [SerializeField] private float timeToSelect;
+    [SerializeField] private float timer;
+    [SerializeField] private bool lookAt;
+    [SerializeField] private GameObject buttonPlay;
+
+    void Update()
+    {
+        if (lookAt)
+        {
+            timer += Time.deltaTime; // menghitung waktu
+            if(timer >= timeToSelect) // membandingkan dengan batas waktu memilih
+            {
+                Debug.Log("Select"); // akan melaksanakan pointer down 
+                ExecuteEvents.Execute(buttonPlay, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+                timer = 0;
+            }
+        }
+        
+    }
+
+    public void PointerEnter() // mendeteksi pointer lagi berada di ui(Button)
+    {
+        lookAt = true;
+        Debug.Log("Masuk");
+    }
+    public void PointerExit() // Mendeteksi Pointer lagi tidak berada di ui(Button)
+    {
+        lookAt = false;
+        Debug.Log("Keluar");
+    }
+    public void LoadScane(int i)// untuk meload scene
+    {
+        SceneManager.LoadScene(i);
+    }
+}
